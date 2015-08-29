6cd2d68a357584904842024cb17c18d67a34b491
sevenwords <jss82386@gmail.com>
2015-08-29 19:46:17 +0900

    first commit

---
+++ src/kr/co/seven/words/HelloIntelliJ.java
@@ -0,0 +1,17 @@
+package kr.co.seven.words;
+
+import org.jetbrains.generate.tostring.util.StringUtil;
+
+/**
+ * Created by Seek on 2015-08-29.
+ */
+public class HelloIntelliJ {
+    public static void main (String [] args) throws Exception {
+        String name = null;
+        System.out.print(StringUtil.isEmpty(name));
+
+
+    }
+}
+
+
